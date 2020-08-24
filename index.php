0242203393844151a644bdeec5c620b6bf1d231d
Filesterk <altemerks@gmail.com>
2020-08-24 19:25:28 +0300

    some require include function

--- index.php
+++ index.php
@@ -9,62 +9,12 @@
 <body>
 
 <div class="container" style="background-color:#d6d6d6; padding-bottom: 20px;">
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style=" border:1px solid #000; margin-bottom: 30px;">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-
-<form action="index.php" method="GET" style=" border:1px solid #000; padding: 20px;">    <!--method="POST"-->
-<div class="form-group">
-    <label for="formGroupExampleInput">Login name</label>
-    <input name="user" type="text" class="form-control" id="formGroupExampleInput" value="Pupkin" placeholder="Login">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input name="password" type="password" class="form-control" id="exampleInputPassword1" value="10kn" placeholder="Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="hidden" name="hello" value="0">
-    <input type="checkbox" name="hello" value="1" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea name="textarea" class="form-control" id="exampleFormControlTextarea1" rows="3">Заполните поле</textarea>
-  </div>
-  <button name="send" type="submit" class="btn btn-primary" value="отправить">Submit</button>
-</form>
+<?php
+require('navbar.php');  //require_once
+?>
+<?php
+include('someform.php');  //include_once
+?>
 </div>
 
 <h1><a href="mysqltable.php">Go to table</a> <br></h1>
@@ -138,34 +88,9 @@ if( !empty($_GET) ) {
 
 <!--Подключение к БД-->
 <?php
-  error_reporting(E_ALL);
-  ini_set(`display_errors`, `on`);
-
-  $host = 'localhost';
-  $user = 'root';
-  $password = '';
-  $dbName = 'test';
-
-  $link = mysqli_connect($host, $user, $password, $dbName) //запрос из БД
-      or die(mysqli_error($link)); //искусственный вывод ошибок SQL
-
-  mysqli_query($link, "SET NAMES 'utf8'"); //задание кодировки
-
-  $query = "SELECT * FROM users WHERE id > 0";
-
-  $result = mysqli_query($link, $query) or die(mysqli_error($link)); //запрос в БД
-
-  var_dump($result);
-
-/*  получение 1 строки из массива
-  $user = mysqli_fetch_assoc($result);
-  var_dump($user);
-*/
-
-  for ($data = []; $row = mysqli_fetch_assoc($result); $data[] = $row);  //получение всего массива
-  var_dump($data);
-  //Parse error: syntax error, unexpected 'or' (T_LOGICAL_OR), expecting end of file in W:\domains\formServer\index.php on line 148
+require_once('includedbtest.php');
 ?>
+
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
