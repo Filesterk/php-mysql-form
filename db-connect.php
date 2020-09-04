63e2951bbb06fa4ee33348a3e750339825a5104c
Filesterk <altemerks@gmail.com>
2020-09-04 09:11:51 +0300

    change func2 funk3

--- db-connect.php
+++ db-connect.php
@@ -29,16 +29,17 @@ function setup_db_connection(string $host, string $user, string $password, strin
  * @return array
  * @todo: Write a function that will get all records from known table
  */
+
+    $link = mysqli_connect($host, $user, $password, $dbName);
+
 function get_all_records(mysqli $link, string $tableName): array
 {
     $host = 'localhost';  
     $user = 'root';    
     $password = ''; 
     $dbName = 'alex_sandbox';
-
-    $link = mysqli_connect($host, $user, $password, $dbName);
-
     $tableName = 'tovar';
+
     if ($result = mysqli_query($link, "SELECT * FROM $tableName", MYSQLI_USE_RESULT)) {
         if (!mysqli_query($link, "SET @a:='this will not work'")) {
             printf("Ошибка: %s\n", mysqli_error($link));
@@ -61,13 +62,11 @@ function get_all_records(mysqli $link, string $tableName): array
         echo $row["name"];
         echo $row["model"];
         echo $row["price"];
-    }*/
-
-    
+    }*/    
     //$result = [$link];
      //Your code !
     //return $result;
-
+    
 
 /**
  * @param int $recordId
@@ -77,22 +76,25 @@ function get_all_records(mysqli $link, string $tableName): array
  */
 function get_record_by_id(int $recordId, string $tableName): array
 {
-    // почитай еще раз про область видимости переменных
-    // Очевидно, что внутри этой функции $link, не определена
-    $link = mysqli_connect($host, $user, $password, $dbName, $port);
-    $result = [$link];
+    $host = 'localhost';  
+    $user = 'root';    
+    $password = ''; 
+    $dbName = 'alex_sandbox';
+    $recordId = 3;
+    $tableName = 'tovar';
 
-    $recordId = 'SELECT model FROM tovar WHERE id = 2';
-    $result = mysql_query($recordId, $link);
+    $link = mysqli_connect($host, $user, $password, $dbName);
+    //$result = [$link];
 
-    while ($row = mysql_fetch_assoc($result)) {
-        echo $row['model'];
+    if ($result = mysqli_query($link, "SELECT * FROM $tableName WHERE $recordId")) {
+        printf("Select single %d record.\n", mysqli_num_rows($result));
+    mysqli_free_result($result);
     }
-
+}
     //$result = [$link];
     // Your code !
-    //return $result;    
-}
+    //return $result;  
+
 
 /**
  * @param array $recordData
@@ -101,13 +103,28 @@ function get_record_by_id(int $recordId, string $tableName): array
  */
 function insert_new_record(array $recordData, string $tableName)
 {
+    $host = 'localhost';  
+    $user = 'root';    
+    $password = ''; 
+    $dbName = 'alex_sandbox';
+    $tableName = 'tovar';
+    $recordData = (`id`, `name`, `model`, `price`);
+    $link = mysqli_connect($host, $user, $password, $dbName);
     // Вот к примеру, я -- пхп. Я вообще не знаю, что такое INSERT, что такое INTO
     //$recordData = INSERT INTO `tovar`(`id`, `name`, `model`, `price`):
     //$tableName = (NULL, `$name`, `$model`, `$price`);
-
-    $sql = "$recordData . VALUES . $tableName";
-
-    if (insert_new_record()->query($sql) === TRUE) {
-        echo "Record created";       
+    if (!$link) {
+        die("Connection failed: " . mysqli_connect_error());
+  }
+   
+  echo "Connected successfully";
+   
+  $sql = "INSERT INTO $recordData VALUES $tableName";
+    if (mysqli_query($link, $sql)) {
+            echo "New record created successfully";
+    } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($link);
     }
+  mysqli_close($link);         
 }
+
